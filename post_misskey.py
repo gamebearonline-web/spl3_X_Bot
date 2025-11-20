e3eb3fc078ae651b2dc45fe9ac52b3fae21be48c
GB <gamebearonline@gmail.com>
2025-11-20 23:18:16 +0900

    Update post_misskey.py

--- post_misskey.py
+++ post_misskey.py
@@ -17,7 +17,7 @@ def post_to_misskey():
     now = datetime.now(jst)
     time_str = now.strftime("🗓️ %Y年%-m月%-d日　%-H時更新")
 
-    text = f"【スプラトゥーン3】スケジュール更新！ \n{time_str}\n画像で全ステージ確認してね！"
+    text = f"【スプラトゥーン3】スケジュール更新！ \n{time_str}\n画像で全ステージ確認してね！\n#スプラ3スケジュール"
 
     # 画像がある場合はアップロード
     files = {}
