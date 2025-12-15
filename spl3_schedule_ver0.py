b0f5138c97541900f8b8bf5757f42fa951bffb36
GB <gamebearonline@gmail.com>
2025-12-15 20:57:13 +0900

    Update spl3_schedule_ver0.py

--- spl3_schedule_ver0.py
+++ spl3_schedule_ver0.py
@@ -519,50 +519,6 @@ FEST_OVERLAY = {
 }
 
 
-
-
-        for idx, slot in enumerate(["now", "next", "next2", "next3", "next4"]):
-            if idx >= len(data):
-                continue
-
-            info = data[idx]
-            cslot = coords[mode][slot]
-
-            st = parse_utc(info["start_time"]).strftime("%H:%M")
-            et = parse_utc(info["end_time"]).strftime("%H:%M")
-
-
-            if "start_time" in cslot:
-                draw_text_with_bg(
-                    draw,
-                    cslot["start_time"],
-                    f"{st}~{et}",
-                    FONT_TIME_NOW if slot == "now" else FONT_TIME_SMALL,
-                    bg_fill=color,
-                )
-
-            stages = info.get("stages", [])
-            for i in [0, 1]:
-                if i >= len(stages):
-                    continue
-                stg = stages[i]
-
-                if f"stage{i}_image" in cslot:
-                    ix, iy, iw, ih = cslot[f"stage{i}_image"]
-                    img = fetch_image(stg["image"]).resize((int(iw), int(ih)))
-                    base.paste(img, (int(ix), int(iy)))
-
-                if f"stage{i}_name" in cslot:
-                    draw_text_with_bg(
-                        draw,
-                        cslot[f"stage{i}_name"],
-                        stg["name"],
-                        FONT_STAGE_NOW if slot == "now" else FONT_STAGE_SMALL,
-                        bg_fill=color,
-                    )
-
-
-
 def render_fest_mode(base, results):
     draw = ImageDraw.Draw(base)
 
@@ -776,6 +732,7 @@ def main():
 # ==========================
 if __name__ == "__main__":
     main()
+
 
 
 
